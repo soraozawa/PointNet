f85cd151e38f83c6202d0ecb05ee81673b943bbd
Sora Ozawa <soraozawa56@gmail.com>
2023-02-25 12:06:52 +0900

    train

--- pointcloud/PointNet/classification/train.py
+++ pointcloud/PointNet/classification/train.py
@@ -1,16 +1,16 @@
+from pathlib import Path
+
 import torch
+from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
 from pointcloud.PointNet.classification.model import PointNetClassification
+from pointcloud.PointNet.data.load_ModelNet import load_ModelNet
 
 num_epoch = 400
 batch_size = 32
 
-device = (
-    torch.device("cuda:0")
-    if torch.cuda.is_available()
-    else torch.device("cpu")
-)
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
 model = PointNetClassification()
 
@@ -21,4 +21,10 @@ scheduler = torch.optim.lr_scheduler.StepLR(
     optimizer, step_size=num_epoch // 4, gamma=0.5
 )
 
-# log_di
+log_dir = Path("data/log_modelnet10_classification")
+log_dir.mkdir(exist_ok=True)
+writer = SummaryWriter(log_dir=log_dir)
+
+train_dataset, test_dataset = load_ModelNet(Path("data/modelnet10/modelnet10"))
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
