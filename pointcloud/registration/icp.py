0f97715f642b7aee61be706fbaacb8588fb8a003
Sora Ozawa <soraozawa56@gmail.com>
2023-04-02 11:28:39 +0900

    source点群とtarget対応点群の共分散行列を求める

--- pointcloud/registration/icp.py
+++ pointcloud/registration/icp.py
@@ -12,7 +12,20 @@ def icp(source_pcd:o3d.geometry.PointCloud, target_pcd:o3d.geometry.PointCloud)-
         idx_list.append(idx[0])
     
     np_target_pcd = np.asarray(target_pcd.points)
+    np_source_pcd = np.asarray(source_pcd.points)
     np_neighbor_pcd = np_target_pcd[idx_list].copy()
     print(idx_list)
     print(np_neighbor_pcd)
+
+    source_pcd_mean = np_source_pcd.mean(axis=0)
+    neighbor_pcd_mean = np_target_pcd.mean(axis=0)
+
+    covar_mat = np.zeros((3,3))
+    for source_point, neighbor_point in zip(np_source_pcd, np_neighbor_pcd):
+        covar_mat += np.dot(source_point.reshape(-1, 1), neighbor_point.reshape(1, -1))
+    covar_mat /= len(np_source_pcd)
+    covar_mat -= np.dot(source_pcd_mean.reshape(-1, 1), neighbor_pcd_mean.reshape(1, -1))
+    print(covar_mat)
+
+
     return transport_mat
