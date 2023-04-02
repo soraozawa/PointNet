055d75bc6aa8ceb422e80925cad0caf463d89158
Sora Ozawa <soraozawa56@gmail.com>
2023-04-02 11:49:59 +0900

    docstring

--- pointcloud/registration/icp.py
+++ pointcloud/registration/icp.py
@@ -24,12 +24,21 @@ def calc_covariance_matrix(pcd1: np.ndarray, pcd2: np.ndarray)->np.ndarray:
     return covar_mat
 
 def icp(source_pcd:o3d.geometry.PointCloud, target_pcd:o3d.geometry.PointCloud)->np.ndarray:
+    """icpアルゴリズムで点群をレジストレーションする
+
+    Args:
+        source_pcd (o3d.geometry.PointCloud): ソース点群（テンプレート）
+        target_pcd (o3d.geometry.PointCloud): ターゲット点群
+
+    Returns:
+        np.ndarray: 回転行列
+    """
     transport_mat = np.eye(4)
 
     pcd_tree = o3d.geometry.KDTreeFlann(target_pcd)
     idx_list = []
     for point in source_pcd.points:
-        [k, idx, _] = pcd_tree.search_knn_vector_3d(point, 1)
+        _, idx, _ = pcd_tree.search_knn_vector_3d(point, 1)
         idx_list.append(idx[0])
 
     np_target_pcd = np.asarray(target_pcd.points)
