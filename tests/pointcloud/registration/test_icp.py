03d484b0d5fdf30b9af860f93cdb3f1d034aa264
Sora Ozawa <soraozawa56@gmail.com>
2023-04-02 11:07:04 +0900

    testcase1

--- tests/pointcloud/registration/test_icp.py
+++ tests/pointcloud/registration/test_icp.py
@@ -1,6 +1,15 @@
-import pytest
+
+import copy
+
+import numpy as np
+import open3d as o3d
 
 from pointcloud.registration.icp import icp
 
+
 def test_icp():
-    assert True
+    source_pcd = o3d.geometry.PointCloud()
+    source_pcd.points = o3d.utility.Vector3dVector(np.array([[0,0,0], [1,1,1]]))
+    target_pcd = copy.deepcopy(source_pcd)
+
+    assert np.array_equal(icp(source_pcd, target_pcd), np.eye(4))
