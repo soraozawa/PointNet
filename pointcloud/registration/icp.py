1ef421870d83bccfe1400e9ac5fc37568d378cc3
Sora Ozawa <soraozawa56@gmail.com>
2023-04-02 11:41:55 +0900

    共分散行列を計算する機能を関数として切り出し

--- pointcloud/registration/icp.py
+++ pointcloud/registration/icp.py
@@ -2,6 +2,26 @@ import numpy as np
 import open3d as o3d
 
 
+def calc_covariance_matrix(pcd1: np.ndarray, pcd2: np.ndarray)->np.ndarray:
+    """2つの点群の共分散行列を求める
+
+    Args:
+        pcd1 (np.ndarray): 点群1
+        pcd2 (np.ndarray): 点群2
+
+    Returns:
+        np.ndarray: 共分散行列
+    """
+    source_pcd_mean = pcd1.mean(axis=0)
+    neighbor_pcd_mean = pcd2.mean(axis=0)
+    covar_mat = np.zeros((3,3))
+    for source_point, neighbor_point in zip(pcd1, pcd2):
+        covar_mat += np.dot(source_point.reshape(-1, 1), neighbor_point.reshape(1, -1))
+    covar_mat /= len(pcd1)
+    covar_mat -= np.dot(source_pcd_mean.reshape(-1, 1), neighbor_pcd_mean.reshape(1, -1))
+
+    return covar_mat
+
 def icp(source_pcd:o3d.geometry.PointCloud, target_pcd:o3d.geometry.PointCloud)->np.ndarray:
     transport_mat = np.eye(4)
 
@@ -10,21 +30,13 @@ def icp(source_pcd:o3d.geometry.PointCloud, target_pcd:o3d.geometry.PointCloud)-
     for point in source_pcd.points:
         [k, idx, _] = pcd_tree.search_knn_vector_3d(point, 1)
         idx_list.append(idx[0])
-    
+
     np_target_pcd = np.asarray(target_pcd.points)
     np_source_pcd = np.asarray(source_pcd.points)
     np_neighbor_pcd = np_target_pcd[idx_list].copy()
-    print(idx_list)
-    print(np_neighbor_pcd)
 
-    source_pcd_mean = np_source_pcd.mean(axis=0)
-    neighbor_pcd_mean = np_target_pcd.mean(axis=0)
 
-    covar_mat = np.zeros((3,3))
-    for source_point, neighbor_point in zip(np_source_pcd, np_neighbor_pcd):
-        covar_mat += np.dot(source_point.reshape(-1, 1), neighbor_point.reshape(1, -1))
-    covar_mat /= len(np_source_pcd)
-    covar_mat -= np.dot(source_pcd_mean.reshape(-1, 1), neighbor_pcd_mean.reshape(1, -1))
+    covar_mat = calc_covariance_matrix(np_source_pcd, np_neighbor_pcd)
     print(covar_mat)
 
 
